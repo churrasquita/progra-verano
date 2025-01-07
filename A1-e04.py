2ec75278317bbedaa26277a2806f8b6b2eef0944
churrasquita <catalina.galleguillos@alumnos.ucn.cl>
2025-01-06 22:07:57 -0300

    A1-e04.py

--- A1-e04.py
+++ A1-e04.py
@@ -5,18 +5,20 @@ Created on Mon Jan  6 12:43:58 2025
 @author: LabCivil1-Pc34
 """
 
-monto = int(input('Ingresa la cantidad de dinero en pesos chilenos (debe ser mayor a $15.000 clp):  '))
-moneda = input('A qué moneda quieres convertirlo? (dólares o euros)').lower()
+monto = float(input('Ingresa la cantidad de dinero en pesos chilenos:  '))
+moneda = input('A qué moneda quieres convertirlo? (dólares o euros) ').lower()
 
+# primero realizamos la comisión para que no se vea afectado el monto 
 comision = 0
-if monto >= 15000 and comision<= 100000:
-    comision = comision* 0.05
+if monto >= 15000 and monto<= 100000:
+    comision = monto* 0.05
 elif monto > 100000:
-    comision = comision * 0.035
+    comision = monto* 0.035
 
-if moneda == 'dolar': 
-    monto = monto/ 1008
+# ahora lo convertimos
+if moneda == 'dólar': 
+    monto = monto/ 1011 #debemos dividir para convertirlo
 elif moneda =='euro':
     monto = monto/900
 
-print(f'la comision fue de {comision} y el monto convertido fue de {monto} en {moneda}')
+print(f'la comision fue de ${comision} clp y el monto convertido fue de {monto} en {moneda}')
