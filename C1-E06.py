668b827032880c4493e7e6fb30832dcd6c396b2b
churrasquita <catalina.galleguillos@alumnos.ucn.cl>
2025-01-06 22:19:27 -0300

    Add files via upload

--- C1-E06.py
+++ C1-E06.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan  6 11:45:31 2025
+Created on Mon Jan  6 22:07:59 2025
 
-@author: LabCivil1-Pc34
+@author: catalina
 """
+#obtener datos
+edad = int(input('Ingrese la edad de la persona: '))
+tiene_cupon = input('¿Tiene cupón?: ').lower()
 
+#calculos 
+if edad <= 3:
+    valor = 0
+elif edad >= 4 and edad <= 12:
+    valor = 5000
+elif edad >= 13 and edad <= 64:
+    valor = 10000
+else:
+    valor = 7500
+
+if tiene_cupon == 'si':
+    valor-=2000
+# else:
+#    valor = valor
+if valor == 0:
+    print('Entra gratis!')
+else:
+    print(f'El precio del ticket es de ${valor}')
