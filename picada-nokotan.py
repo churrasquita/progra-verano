79f84b7e8dd41708c600c74e42e65908e387dc47
churrasquita <catalina.galleguillos@alumnos.ucn.cl>
2025-01-07 23:20:08 -0300

    P1 2024
    El programa debe mostrar el listado de platos que ofrece: Arroz de Bashame a $2.500, Sopa de Astas a $1.500 y
    Galletas Shika a $1.000. Luego, se debe permitir al usuario ingresar el nombre de cada uno de los platos solicitados
    (ARROZ, SOPA y GALLETAS respectivamente) hasta que ingrese la opción de pago (PAGAR). Cuando se ingrese
    pagar, se debe mostrar el monto total del pedido en base a los platos que se agregaron a este.
    Luego de mostrar el monto total del pedido, se debe preguntar si el usuario desea registrar un nuevo pedido. En
    caso afirmativo (SI), se debe repetir el proceso indicado anteriormente. En caso contrario (NO), el programa deja
    de preguntar por más pedidos, imprime el resumen del día y termina su ejecución. Sin embargo, la dueña del
    local explicó que algunos clientes pueden obtener un descuento del 10% en su pedido si dicen la palabra clave
    (NUN!). Por lo tanto, al momento de preguntar si se desea procesar un nuevo pedido, si el usuario ingresa dicha
    palabra clave, se debe aplicar el descuento, mostrar el nuevo monto del pedido por pantalla y volver a preguntar
    por segunda vez si desea procesar un nuevo pedido. El descuento solo se aplica una vez por pedido.
    Dentro del resumen del día, se solicita que se muestre el total de pedidos realizados, la ganancia resultante de
    todos los pedidos (restando los descuentos), el pedido que más valor aportó a la ganancia (pedido más costoso),
    el plato menos solicitado (considerando todos los pedidos) y la cantidad de veces que se aplicó el descuento.

---
+++ picada-nokotan.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 21:14:53 2025
+
+@author: catal
+"""
+contador_pedidos = 1
+print('Bienvenido a la picada de Nokotan!')
+
+
+procesar_pedidos = 'SI'
+ganancias = 0
+mas_costoso = -11111
+valor_mascostoso = -111
+aplica_descuento = 0
+
+cont_arroz= 0
+cont_sopa = 0
+cont_galletas= 0
+menor_global= 9999999
+plato_menos_pedido = ''
+
+while procesar_pedidos != 'NO':
+    print('     ')
+    print(f'PEDIDO #{contador_pedidos}!')
+    print('MENU DE LA PICADA')
+    print('[ARROZ] Arroz de Bashame ($2500)')
+    print('[SOPA] Sopa de Astas ($1500)')
+    print('[GALLETAS] Galletas Shika ($1000)')
+    print('[PAGAR] Realizar pago del pedido')
+    
+    plato = input('Selecciona un plato: ').upper()
+    valor_pedido = 0
+    
+    while plato != 'PAGAR':
+        if plato == 'ARROZ':
+            valor_pedido += 2500
+            cont_arroz += 1
+    
+        elif plato == 'SOPA':
+            valor_pedido +=1500
+            cont_sopa += 1
+        
+        else:
+            valor_pedido += 1000
+            cont_galletas += 1
+        
+        plato = input('Selecciona un plato: ').upper()
+    
+    ganancias += valor_pedido
+    print(f'Total del pedido: ${valor_pedido}')
+    procesar_pedidos = input('Desea seguir procesando pedidos: ').upper()
+    if procesar_pedidos == 'NUN!': 
+        ganancias -= (valor_pedido*0.10)
+        valor_pedido*=0.90
+        print('Excelente! Se ha aplicado el descuento del 10%')
+        print(f'Nuevo total del pedido: ${valor_pedido}')
+        aplica_descuento += 1
+        procesar_pedidos = input('Desea seguir procesando pedidos: ').upper()
+        
+    if valor_pedido>valor_mascostoso:
+        mas_costoso = contador_pedidos
+        valor_mascostoso = valor_pedido
+    
+    contador_pedidos += 1 
+    
+if cont_arroz < cont_sopa and cont_arroz < cont_galletas:
+    plato_menos_pedido = "ARROZ"
+    menor_global = cont_arroz
+elif cont_sopa< cont_arroz and cont_sopa<cont_galletas:
+    plato_menos_pedido = "SOPA"
+    menor_global = cont_sopa
+else:
+    plato_menos_pedido = "GALLETAS"
+    menor_global = cont_galletas
+
+
+    
+print('     ')
+print('RESUMEN DEL DIA!')
+print(f'Pedidos realizados: {contador_pedidos-1}')
+print(f'Ganancia: ${ganancias}')
+print(f'Pedido más costoso: #{mas_costoso} (${valor_mascostoso})')
+print(f'Plato menos pedido: {plato_menos_pedido} ({menor_global})')
+print(f'Num. veces canje de descuento: {aplica_descuento}')
